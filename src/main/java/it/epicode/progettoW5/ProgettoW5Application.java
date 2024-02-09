f77b3994f7df5524ba601d9d3ae6f6cd233042d6
lizzlyna <barbara.panzetta@gmail.com>
2024-02-09 13:13:05 +0100

    primo commit

---
+++ src/main/java/it/epicode/progettoW5/ProgettoW5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.progettoW5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoW5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoW5Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/progettoW5/ProgettoW5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.progettoW5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoW5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
